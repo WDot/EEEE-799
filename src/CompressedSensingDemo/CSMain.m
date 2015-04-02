46511afe59bbc53f6130ae5030add406d750302c
WDot <mad6384@rit.edu>
2015-04-01 23:09:01 -0400

    Initial demo with L1 optimizer
    Just shows that the idea works

--- src/CompressedSensingDemo/CSMain.m
+++ src/CompressedSensingDemo/CSMain.m
@@ -1,13 +1,22 @@
 BLOCK_SIZE = 160;
 THRESHOLD  = 0.005;
+SAMPLE_SEQUENCE = 0:(BLOCK_SIZE - 1);
 [pathstr,~,~] = fileparts(mfilename('fullpath'));
 [testVector, Fs] = audioread(strcat(pathstr,'/../../testvectors/test.wav'));
 testVector = testVector(1:(floor(length(testVector) / BLOCK_SIZE) * BLOCK_SIZE));
-testVector = testVector(1:160);
+testVector = testVector(1:BLOCK_SIZE);
 representationBasis = dctmtx(BLOCK_SIZE);
 samplingBasis = randn(BLOCK_SIZE);
 sparseVector = representationBasis * testVector;
 sparseVector(abs(sparseVector) <= THRESHOLD) = 0;
 S = length(find(sparseVector > 0)); %Calculate S-sparseness
 coherence = Coherence(representationBasis,samplingBasis,BLOCK_SIZE);
-necessarySamples = coherence^2 * S * log(BLOCK_SIZE);
+m = 110;
+sampleRows = randperm(BLOCK_SIZE);
+sampleRows = sampleRows(1:m);
+underSampledVector = samplingBasis(sampleRows,:) * sparseVector;
+opts = optimoptions('fmincon','Algorithm','interior-point');
+x = fmincon(@(x)norm(x,1),zeros(1,BLOCK_SIZE),[],[],...
+    samplingBasis(sampleRows,:),underSampledVector,-Inf,Inf,[],opts);
+plot(SAMPLE_SEQUENCE,sparseVector,SAMPLE_SEQUENCE,x);
+legend('Original','Reconstructed');
