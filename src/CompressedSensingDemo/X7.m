0d41e94650de34121e5b7a485194cf75c7c59d75
WDot <mad6384@rit.edu>
2015-04-11 14:27:26 -0400

    Attempt at X7,, seems to make sense, we'll see

--- src/CompressedSensingDemo/X7.m
+++ src/CompressedSensingDemo/X7.m
@@ -1,4 +1,10 @@
 function X = X7( k, n )
     y = X1(k,n);
+    A = zeros(n, n);
+    for i = 1:(length(A) - 1)
+        A(i,i:(i+1)) = [-1, 1];
+    end
+    A(length(A),length(A)) = -1;
+    X = linsolve(A,y');
 end
 
